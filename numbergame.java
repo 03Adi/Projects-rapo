fab6a619ad949151c265681dae73fab691058dc8
adityapandey03012003@gmail.com <104114546+03Adi@users.noreply.github.com>
2023-09-28 09:24:03 +0530

    Create numbergame.java

---
+++ numbergame.java
@@ -0,0 +1,76 @@
+//Number Guessing Game
+
+import java.util.Scanner;
+import java.util.Random;
+
+class Game{
+	public static void main(String args[])
+    { 
+    		generateRandom();
+    } 
+
+
+    public static void generateRandom(){
+    
+    	Random rand=new Random();
+
+    	//Generating a random number between 0-111
+    	int randomNum=rand.nextInt(111);
+
+    	guess(randomNum);
+    }
+
+
+
+    public static void guess(int randomNum){
+    	
+    	Scanner in=new Scanner(System.in);
+
+    	
+    	System.out.println("");
+    	System.out.println("Number Guessing Game");
+    	System.out.print("Guess a number between 0-111: ");
+    	int guess=in.nextInt();
+    	System.out.println("");
+
+    
+    	while (guess<0 || guess>111){
+    		System.out.print("Guess a number between 0-111: ");
+    		guess=in.nextInt();
+    		System.out.println("");
+    	}
+
+    	
+    	int tries=0;
+    	while(guess!=randomNum){
+    		tries++;
+    		System.out.println("Wrong Guess!");
+    		System.out.print("Guess again: ");
+    		guess=in.nextInt();
+    		System.out.println("");
+
+    		
+    		while (guess<0 || guess>111){
+    			System.out.print("Guess a number between 0-111: ");
+    			guess=in.nextInt();
+    			System.out.println("");
+    		}
+    	}
+
+    	
+    	System.out.println("Correct Answer. You Won!");
+    	System.out.println("Wrong Tries: "+tries);
+    	System.out.println("");
+
+    	
+    	System.out.println("Press 1 to Play again.");
+    	System.out.println("Press 0 to exit.");
+    	int choice=in.nextInt();
+
+    	if(choice==1)
+    		generateRandom();
+    	else
+    		return;
+    }
+
+}
